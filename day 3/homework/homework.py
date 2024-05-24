61e3cc29e7d6a73b9a0e3dfc0d02e7626d81deb3
xurc21dze <giyoawesome@gmail.com>
2024-05-24 22:38:37 +0400

    first commit

---
+++ day 3/homework/homework.py
@@ -0,0 +1,55 @@
+print("hello")
+
+
+age = "13"
+
+name = "giorgi"
+
+last_name = "xurcidze"
+
+hobby = "drawing"
+
+location = "tbilisi"
+
+print (age)
+print (name)
+print (last_name)
+print (hobby)
+print(location)
+
+print(13 + 91)
+print(34 + 12)
+print(16 + 33)
+print(104 + -2)
+print(122 + 67)
+print(143 + -19)
+print(19 + 234)
+print(122 + 3)
+print(88 + 33)
+print( 12 + 22)
+
+print(22 // 10)
+print(-700 // 2/23)
+print(600 // 25)
+print(122 // 3)
+print(34 // 44.2)
+print(1029 // 2)
+print(233 // 344)
+print(12 // 44)
+print(90 // 55)
+print(55 // 15)
+
+print(23 - 4)
+print(67 - 12)
+print(121 - 45)
+print(3434 - 44)
+print(899 - 232)
+print(111 - 309)
+print(34 - 11)
+print(44 - 10)
+print(120 - 1)
+print(202 - 19)
+
+print(19 * 2)
+print(20 * 33)
+print(55 * 5)
