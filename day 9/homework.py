a32487d1e5161dd087b1d27d6ac4693a5e561685
xurc21dze <giyoawesome@gmail.com>
2024-06-12 21:29:57 +0400

    seventh commit

---
+++ day 9/homework.py
@@ -0,0 +1,33 @@
+1. #Tell the user to input 2 different numbers and then add them together
+
+input(int(2))
+input(int(18))
+input(int(2 + 18))
+
+2. #Tell the user to create an input that finds the area of a square, but you must know the height and width
+
+
+#height = 12
+#width = 11
+input(int(12))
+input(int(11))
+#S = 11 * 12
+print(11 * 12)
+
+3. #Tell the user to create a number and print 2 times this number
+
+input(int(12))
+print(12 * 2)
+
+4. #Tell the user to print the summmary of all the sides of a rectangle, but show the sides first
+
+input(int(6))
+input(int(6))
+input(int(4))
+input(int(4))
+print(6 + 6 + 4 + 4)
+
+5. #Tell the user to input a number and then print the square of this number
+
+input(int(13))
+print(13 ** 2)
