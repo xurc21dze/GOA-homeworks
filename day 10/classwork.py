853ad3b283aebb2693d83f550775771305ccc858
xurc21dze <giyoawesome@gmail.com>
2024-06-16 22:41:47 +0400

    eighth commit

---
+++ day 10/classwork.py
@@ -0,0 +1,23 @@
+savings = input(150)
+
+input =  150
+
+fee =  7.5
+
+input(str(150 + 7.5))
+
+message = 150 + 7.5
+
+print(message)
+
+savings = input(250)
+
+input2 = 250
+
+fee2 = 12.5
+
+input(str(250 + 12.5))
+
+message2 = 250 + 12.5
+
+print(message2)
