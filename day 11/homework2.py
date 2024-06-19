d3e3059a4ee9f66c3c673556811ddaca69d3ba81
xurc21dze <giyoawesome@gmail.com>
2024-06-19 18:13:34 +0400

    ninth commit

---
+++ day 11/homework2.py
@@ -0,0 +1,11 @@
+#tell the user to input the first users age
+
+input(str("we need the first persons age"))
+input(str("then we need the second persons age"))
+
+liza = 13
+nika = 8
+
+print(13 > 8)
+
+input(str("this statement is true"))
