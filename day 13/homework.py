013af8369962816523d02616d84baa0b74432fa3
xurc21dze <giyoawesome@gmail.com>
2024-06-27 17:49:42 +0400

    tenth commit

---
+++ day 13/homework.py
@@ -0,0 +1,18 @@
+
+print(True or True or False)
+
+print(False or False or True)
+
+print(False or False)
+
+print(True or True or True)
+
+print(True or False or True or False)
+
+print(True or False or True)
+
+print(False or False or False)
+
+print(False or False)
+
+print(True or False or False)
