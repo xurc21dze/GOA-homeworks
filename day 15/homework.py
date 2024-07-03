c836c4a27be6251e068ae3595688f2c41eb3d260
xurc21dze <giyoawesome@gmail.com>
2024-07-03 18:45:28 +0400

    twelveth commit

---
+++ day 15/homework.py
@@ -0,0 +1,35 @@
+
+#homework 1
+
+input("enter a random number between 1 and 1000")
+
+number = 302
+
+input(number < 302)
+input(number > 302)
+
+#homework 2
+
+toy = 9
+
+car = 12
+
+input(car + toy + toy)
+
+cost = car + toy + toy
+
+print("cost")
+
+#homework 3
+
+weight = 63
+
+height = 170
+
+BMI = 63 / (170 * 170)
+
+if BMI < 18.5. print:("underweight")
+
+if BMI > 18.5 < 24.9. print:("normal weight")
+
+if BMI > 24.9. print:("overweight")
---
+++ day 15/homework1.py
@@ -0,0 +1,14 @@
+
+
+
+print(12 > 123)
+print(89 > 22)
+print(38 > 0)
+print(183 < 23)
+print(938 < 550)
+print(129 < 33)
+print(120 > 22)
+print(37 < 1)
+print(99 < 100)
+print(129 > 2)
+
