f54f218a77a9e5bb97c5f7a9a8622a807021b9ec
xurc21dze <giyoawesome@gmail.com>
2024-07-14 19:05:48 +0400

    fourteenth commit

---
+++ day 17/homework.py
@@ -0,0 +1,36 @@
+
+
+#homework 1
+
+for i in range(30):
+    print("GOABEST")
+
+# homework 2
+
+for i in range(5, 151):
+    print(i)
+
+#homework 3
+
+for i in range(2, 51):
+    print(i + 4)
+
+#homework 4
+
+for i in range(10):
+    print(i + "GOA")
+
+#homework 5
+
+for i in range(2, 3, 4, 5, 6, 7, 8, 9, 10):
+    print(i)
+
+#homework 6
+
+input(4)
+
+while 4 < 5 and 4 > 3:
+    print("+")
+
+while 4 > 5 and 4 < 3:
+    print("-")
