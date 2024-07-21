0a5177f18049aae447489fcab92c76af1870b0f6
xurc21dze <giyoawesome@gmail.com>
2024-07-21 17:47:04 +0400

    sixteenth commit

---
+++ day 19/homework.py
@@ -0,0 +1,24 @@
+#homework 1
+
+for i in range(126, 0, -1):
+    print (i)
+
+
+#homework 2
+
+
+x = 1
+
+x += 10
+
+for i in range(48):
+    print(x)
+
+for i in range(48):
+    print("3 *", i, "=", 3 * i)
+
+
+#homework 3
+
+while True:
+    print(1, 100)
